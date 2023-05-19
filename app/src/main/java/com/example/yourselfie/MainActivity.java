50becb94cfa9e086a7bd2de29079a5482ab2185c
linn.mann@gmail.com <linn.mann@gmail.com>
2023-05-19 11:19:32 +0200

    Add functionality to grab current client to be able to show username

--- app/src/main/java/com/example/yourselfie/MainActivity.java
+++ app/src/main/java/com/example/yourselfie/MainActivity.java
@@ -3,12 +3,29 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
+ public class MainActivity extends AppCompatActivity {
+
+    TextView userName;
+    GoogleSignInClient googleClient;
+    GoogleSignInOptions googleOptions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userName = findViewById(R.id.userName);
+        googleOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleClient = GoogleSignIn.getClient(this, googleOptions);
+        GoogleSignInAccount gAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (gAccount != null){
+            String gName = gAccount.getDisplayName();
+            userName.setText(gName);
+        }
     }
 }
