d677602c233877effb9f293982615778a99d1280
linn.mann@gmail.com <linn.mann@gmail.com>
2023-05-25 19:43:19 +0200

    Move camera functionality to photo activity

--- app/src/main/java/com/example/yourselfie/MainActivity.java
+++ app/src/main/java/com/example/yourselfie/MainActivity.java
@@ -1,49 +1,32 @@
  package com.example.yourselfie;
 
-import static android.content.ContentValues.TAG;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.view.PreviewView;
-import androidx.core.content.ContextCompat;
 
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.common.util.concurrent.ListenableFuture;
 
-import java.util.Arrays;
-import java.util.Map;
 
- public class MainActivity extends AppCompatActivity {
-
-     private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA"};
-     TextView userName;
-
-     PreviewView previewView;
+ public class MainActivity extends AppCompatActivity { TextView userName;
     GoogleSignInClient googleClient;
     GoogleSignInOptions googleOptions;
 
-    private Button cameraButton;
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        previewView = findViewById(R.id.previewView);
-        cameraButton = findViewById(R.id.camera_button);
+        Button cameraButton = findViewById(R.id.camera_button);
         userName = findViewById(R.id.userName);
         googleOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         googleClient = GoogleSignIn.getClient(this, googleOptions);
@@ -54,63 +37,10 @@ import java.util.Map;
             userName.setText(gName);
         }
 
-        ActivityResultLauncher<String[]> activityResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.RequestMultiplePermissions(),
-                new ActivityResultCallback<Map<String, Boolean>>() {
-                    @Override
-                    public void onActivityResult(Map<String, Boolean> permissions) {
-                        // Handle Permission granted/rejected
-                        boolean permissionGranted = true;
-                        for (Map.Entry<String, Boolean> entry : permissions.entrySet()) {
-                            String key = entry.getKey();
-                            boolean value = entry.getValue();
-                            if (Arrays.asList(REQUIRED_PERMISSIONS).contains(key) && !value) {
-                                permissionGranted = false;
-                            }
-                        }
-                        if (!permissionGranted) {
-                            Toast.makeText(getApplicationContext(),
-                                    "Permission request denied",
-                                    Toast.LENGTH_SHORT).show();
-                        } else {
-                            startCamera();
-                        }
-                    }
-                }
-        );
-
         cameraButton.setOnClickListener(view -> {
-         activityResultLauncher.launch(REQUIRED_PERMISSIONS);
+            Intent intent = new Intent(MainActivity.this, PhotoActivity.class);
+            startActivity(intent);
      });
     }
-
-     private void startCamera() {
-
-         ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-
-         cameraProviderFuture.addListener(() -> {
-             try {
-                 // Used to bind the lifecycle of cameras to the lifecycle owner
-                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-
-                 // Preview
-                 Preview preview = new Preview.Builder().build();
-                 preview.setSurfaceProvider(previewView.getSurfaceProvider());
-
-                 // Select front camera as a default
-                 CameraSelector cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA;
-
-                 // Unbind use cases before rebinding
-                 cameraProvider.unbindAll();
-
-                 // Bind use cases to camera
-                 cameraProvider.bindToLifecycle(this, cameraSelector, preview);
-             } catch (Exception e) {
-                 Log.e(TAG, "Use case binding failed", e);
-             }
-         }, ContextCompat.getMainExecutor(this));
-     }
-
-
 
  }
--- app/src/main/java/com/example/yourselfie/PhotoActivity.java
+++ app/src/main/java/com/example/yourselfie/PhotoActivity.java
@@ -1,26 +1,148 @@
-/*package com.example.yourselfie;
+package com.example.yourselfie;
+
+import static android.content.ContentValues.TAG;
+
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 
-import android.content.Intent;
+import android.content.ContentValues;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Map;
 
 public class PhotoActivity extends AppCompatActivity {
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA"};
+    ImageCapture imageCapture;
+    PreviewView previewView;
+    Button photoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        previewView = findViewById(R.id.previewView);
+        photoButton = findViewById(R.id.photo_button);
+
+
+        ActivityResultLauncher<String[]> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.RequestMultiplePermissions(),
+                permissions -> {
+                    // Handle Permission granted/rejected
+                    boolean permissionGranted = true;
+                    for (Map.Entry<String, Boolean> entry : permissions.entrySet()) {
+                        String key = entry.getKey();
+                        boolean value = entry.getValue();
+                        if (Arrays.asList(REQUIRED_PERMISSIONS).contains(key) && !value) {
+                            permissionGranted = false;
+                        }
+                    }
+                    if (!permissionGranted) {
+                        Toast.makeText(getApplicationContext(),
+                                "Permission request denied",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        startCamera();
+                    }
+                }
+        );
+
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS);
+        photoButton.setOnClickListener(view -> takePhoto());
     }
 
+    private void startCamera() {
+
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                // Used to bind the lifecycle of cameras to the lifecycle owner
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                // Preview
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+                // Select front camera as a default
+                CameraSelector cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA;
+
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll();
+
+                imageCapture =
+                        new ImageCapture.Builder().setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                                .setTargetRotation(previewView.getDisplay().getRotation())
+                                .build();
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
+            } catch (Exception e) {
+                Log.e(TAG, "Use case binding failed", e);
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void takePhoto() {
+
+        // Create time stamped name and MediaStore entry.
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US)
+                .format(System.currentTimeMillis());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+        contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/YourSelfie");
+
+        // Create output options object which contains file + metadata
+        ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions.Builder(
+                getContentResolver(),
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+                .build();
+
+        // Set up image capture listener, which is triggered after photo has been taken
+        imageCapture.takePicture(
+                outputOptions,
+                ContextCompat.getMainExecutor(this),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exc) {
+                        Log.e(TAG, "Photo capture failed: " + exc.getMessage(), exc);
+                    }
+
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults output) {
+                        String msg = "Photo capture succeeded: " + output.getSavedUri();
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, msg);
+                    }
+                }
+        );
+    }
+
+}
 
 
-    //Saving photo/creating image with date and time
+   /* //Saving photo/creating image with date and time
     String currentPhotoPath;
 
     private File createImageFile() throws IOException {
